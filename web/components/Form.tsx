d479ba7dd79439441c0c2de4923865435207f8d0
sicko <z1599137080@gmail.com>
2021-11-27 23:13:20 +0800

    init

--- web/components/Form.tsx
+++ web/components/Form.tsx
@@ -59,17 +59,14 @@ const A = styled.a`
 export const Form: React.FC<FormProps> = () => {
 	const Submit = async (e) => {
 		e.preventDefault()
-		const result = await fetch(
-			`http://${window.location.hostname}:9090/upload`,
-			{
-				method: 'POST',
-				body: JSON.stringify(formData),
-				headers: {
-					'Content-Type': 'application/json',
-					Accept: 'application/json',
-				},
-			}
-		)
+		const result = await fetch(`http://${window.location.hostname}/`, {
+			method: 'POST',
+			body: JSON.stringify(formData),
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		})
 		const res = await result.json()
 		console.log(res)
 	}
