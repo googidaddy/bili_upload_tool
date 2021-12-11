1881b06534ba86087f4d34acb93276307151d349
sicko <z1599137080@gmail.com>
2021-12-11 22:22:10 +0800

    fixed

--- Docker-main/main/download.py
+++ Docker-main/main/download.py
@@ -17,6 +17,25 @@ class MyCustomPP(yt_dlp.postprocessor.PostProcessor):
     def run(self, info):
         self.to_screen('Finished downloading, now uploading ...')
         return [], info
+def format_selector(ctx):
+
+    formats = ctx.get('formats')[::-1]
+
+    best_video = next(f for f in formats
+                      if f['vcodec'] != 'none' and f['acodec'] == 'none')
+
+
+    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
+
+    best_audio = next(f for f in formats if (
+        f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
+
+    yield {
+        'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
+        'ext': best_video['ext'],
+        'requested_formats': [best_video, best_audio],
+        'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
+    }
 
 ydl_opts = {
     'format': format_selector,
